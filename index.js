ce4e4faf66294a1998c41dbfdb93cdc6778ed639
ZCPower <zachpowerxc@gmail.com>
2022-06-08 20:48:14 -0500

    Finished Part 2 section 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// inside index.js
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
+
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
